c833ec06074e4273487818cec24c5f5ee11536a8
narasimha <narasimha34327@gmail.com>
2024-12-18 09:55:01 +0530

    changes done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PokemonNavigator from './PokemonNavigator'
+
+function App() {
+
+
+  return (
+    <div>
+      <PokemonNavigator/>
+    </div>
+  )
+}
+
+export default App
